ddd6dc90783a2eda8995183b7eaf817185a086c7
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-04-15 16:54:17 +0200

    [add] stampa senza stili

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ Esercizio di oggi: PHP Hotel
 **Descrizione**
 Partiamo da questo array di hotel. https://www.codepile.net/pile/OEWY7Q1G
 1. Stampare tutti i nostri hotel con tutti i dati disponibili.
-- stampo i dati in pagina 
+- stampo i dati in pagina con l'uso di un ciclo
 - aggiunta bootstrap
 - sistemare le informazioni con una tabella
---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+$hotels = [
+
+  [
+      'name' => 'Hotel Belvedere',
+      'description' => 'Hotel Belvedere Descrizione',
+      'parking' => true,
+      'vote' => 4,
+      'distance_to_center' => 10.4
+  ],
+  [
+      'name' => 'Hotel Futuro',
+      'description' => 'Hotel Futuro Descrizione',
+      'parking' => true,
+      'vote' => 2,
+      'distance_to_center' => 2
+  ],
+  [
+      'name' => 'Hotel Rivamare',
+      'description' => 'Hotel Rivamare Descrizione',
+      'parking' => false,
+      'vote' => 1,
+      'distance_to_center' => 1
+  ],
+  [
+      'name' => 'Hotel Bellavista',
+      'description' => 'Hotel Bellavista Descrizione',
+      'parking' => false,
+      'vote' => 5,
+      'distance_to_center' => 5.5
+  ],
+  [
+      'name' => 'Hotel Milano',
+      'description' => 'Hotel Milano Descrizione',
+      'parking' => true,
+      'vote' => 2,
+      'distance_to_center' => 50
+  ],
+
+];
+?>
+
+<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.css' integrity='sha512-VcyUgkobcyhqQl74HS1TcTMnLEfdfX6BbjhH8ZBjFU9YTwHwtoRtWSGzhpDVEJqtMlvLM2z3JIixUOu63PNCYQ==' crossorigin='anonymous'/>
+    <link rel="stylesheet" href="style.css">
+
+    <title>PHP Hotel</title>
+  </head>
+  <body>
+
+  <div class="container">
+      <?php foreach ($hotels as $hotel) : ?>
+        <p> <?php echo $hotel['name'] ?> </p>
+        <p> <?php echo $hotel['description']  ?> </p>
+        <p> <?php echo $hotel['parking'] ?> </p>
+        <p> <?php echo $hotel['vote']  ?> </p>
+        <p> <?php echo $hotel['distance_to_center'] ?> </p>
+        <?php endforeach ?>
+        
+  </div>
+  </body>
+</html>
